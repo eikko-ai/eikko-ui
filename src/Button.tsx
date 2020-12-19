60e89fca1e690ecc030004257efa86fcea2c2f09
Nuno Freire <greven.pt@gmail.com>
2020-12-19 17:34:12 +0000

    Remove monorepo config

--- src/Button.tsx
+++ src/Button.tsx
@@ -7,7 +7,7 @@ type types = {
 }
 
 const Button = styled.button(({ isPrimary, isSecondary, isSmall }: types) => [
-  tw`text-lg px-4 py-2 rounded-lg focus:outline-none border-2 border-primary-900 bg-primary-500 text-primary-50`
+  tw`text-lg px-4 py-2 bg-red-500 text-red-50`
 ])
 
 export default Button
