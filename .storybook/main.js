914518e99850a5262a89a9b6cc449f5e1b602f81
Nuno Freire <greven.pt@gmail.com>
2020-12-19 23:28:10 +0000

    Update

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,8 +2,13 @@ const path = require('path')
 
 module.exports = {
   stories: ['../components/**/*.stories.mdx', '../components/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-docs',
+    '@storybook/addon-controls'
+  ],
   logLevel: 'debug',
-  addons: [],
   webpackFinal: (config) => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,18 +1,19 @@
 import { h } from 'preact'
-import tw, { styled } from 'twin.macro'
+// import tw, { styled } from 'twin.macro'
 
 export type ButtonVariant = 'solid' | 'secondary' | 'outline' | 'ghost' | 'link' | 'unstyled'
 
-const StyledButton = styled.button(() => [
-  tw`text-lg px-8 py-2 rounded focus:outline-none`,
-  tw`transform transition-transform duration-75`
-])
+// const StyledButton = styled.button(() => [
+//   tw`text-lg px-8 py-2 rounded focus:outline-none`,
+//   tw`transform transition-transform duration-75`
+// ])
 
 interface ButtonProps {
   /** Controls the basic button style. */
   variant?: ButtonVariant
 }
 
-export const Button: React.FC<ButtonProps> = ({ children, variant }) => {
-  return <StyledButton>{children}</StyledButton>
+export const Button: React.FC<ButtonProps> = ({ children }) => {
+  // return <StyledButton>{children}</StyledButton>
+  return <button>{children}</button>
 }
