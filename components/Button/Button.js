b787b36c081b3308aa304c9e93890c32a4739ac4
Nuno Freire <greven.pt@gmail.com>
2020-12-20 00:32:16 +0000

    Update

--- components/Button/Button.js
+++
@@ -1,9 +0,0 @@
-import { h } from 'preact';
-import tw, { styled } from 'twin.macro';
-const StyledButton = styled.button(() => [
-    tw `text-lg px-8 py-2 rounded focus:outline-none`,
-    tw `transform transition-transform duration-75`
-]);
-export const Button = ({ children, variant }) => {
-    return h(StyledButton, null, children);
-};
--- components/Button/Button.stories.js
+++
@@ -1,4 +0,0 @@
-import { h } from 'preact';
-import { Button } from './Button';
-export default { title: 'Button' };
-export const Normal = () => h(Button, null, "Normal");
