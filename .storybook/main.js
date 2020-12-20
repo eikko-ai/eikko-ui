9e6d43bcf4ae4eb4c1c123852ff8b74b8f6d8e94
Nuno Freire <greven.pt@gmail.com>
2020-12-20 00:13:05 +0000

    Update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,3 @@
-const path = require('path')
-
 module.exports = {
   stories: ['../components/**/*.stories.mdx', '../components/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -7,19 +5,5 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-docs',
     '@storybook/addon-controls'
-  ],
-  logLevel: 'debug',
-  webpackFinal: (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve('ts-loader')
-        }
-      ],
-      include: [path.resolve(__dirname, '../components')]
-    })
-    config.resolve.extensions.push('.ts', '.tsx')
-    return config
-  }
+  ]
 }
