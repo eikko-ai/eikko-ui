90c6a29dd58262a94407cf0defa7a150703dec4d
Nuno Freire <greven.pt@gmail.com>
2020-12-22 17:12:12 +0000

    Update structure

--- packages/playground/src/components/Header.tsx
+++ packages/playground/src/components/Header.tsx
@@ -3,19 +3,19 @@ import 'twin.macro'
 
 export const Header = () => {
   return (
-    <header tw="text-gray-300 bg-trueGray-800">
-      <div tw="container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center">
-        <a tw="flex font-medium items-center text-gray-100 mb-4 md:mb-0">
-          <span tw="ml-3 text-xl">
-            <a href="/">Kai UI</a>
-          </span>
-        </a>
-        <nav tw="md:ml-auto flex flex-wrap items-center text-base justify-center hover:cursor-pointer">
-          <a tw="mr-5 hover:text-gray-600">Home</a>
-          <a tw="mr-5 hover:text-gray-600">Second Link</a>
-          <a tw="mr-5 hover:text-gray-600">Third Link</a>
-        </nav>
-      </div>
-    </header>
+    // <header tw="text-gray-300 bg-trueGray-800">
+    //   <div tw="container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center">
+    //     <a href="/" tw="flex font-medium items-center text-gray-100 mb-4 md:mb-0">
+    //       <span tw="ml-3 text-xl">Kai UI</span>
+    //     </a>
+    //     <nav tw="md:ml-auto flex flex-wrap items-center text-base justify-center hover:cursor-pointer">
+    //       <a tw="mr-5 hover:text-gray-600">Home</a>
+    //       <a tw="mr-5 hover:text-gray-600">Second Link</a>
+    //       <a tw="mr-5 hover:text-gray-600">Third Link</a>
+    //     </nav>
+    //   </div>
+    // </header>
+
+    <header>Header</header>
   )
 }
--- packages/playground/src/routes/Buttons.tsx
+++ packages/playground/src/routes/Buttons.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import 'twin.macro'
-import { Button } from '@kai/components'
+// import { Button } from '@kai/components'
 
 export const Buttons = () => {
   return (
-    <div tw="flex flex-col items-center justify-center h-screen bg-black text-white">
-      <h1>Buttons</h1>
-      <div tw="flex flex-col items-center justify-center">
-        <Button>Save</Button>
-        <a href="/">Back</a>
-      </div>
-    </div>
+    // <div tw="flex flex-col items-center justify-center h-screen bg-black text-white">
+    //   <h1>Buttons</h1>
+    //   <div tw="flex flex-col items-center justify-center">
+    //     <Button>Save</Button>
+    //     <a href="/">Back</a>
+    //   </div>
+    // </div>
+
+    <div>Button</div>
   )
 }
