da6302fdd6618bc710b3c9181e525b077fba19f1
Nuno Freire <greven.pt@gmail.com>
2020-12-22 21:00:47 +0000

    Restore layouts

--- packages/playground/src/routes/Buttons.tsx
+++ packages/playground/src/routes/Buttons.tsx
@@ -1,30 +1,15 @@
 import React from 'react'
-import tw, { styled } from 'twin.macro'
+import 'twin.macro'
 import { Button } from '@kai/components'
 
-const StyledDiv = styled.div`
-  color: white;
-  ${tw`bg-red-300 p-4 hover:bg-blue-300`}
-`
-
 export const Buttons = () => {
-  // return (
-  //   <div tw="flex flex-col items-center justify-center h-screen bg-black text-white">
-  //     <h1>Buttons</h1>
-  //     <div tw="flex flex-col items-center justify-center">
-  //       <Button>Save</Button>
-  //       <a href="/">Back</a>
-  //     </div>
-  //   </div>
-  // )
-
   return (
-    <div>
-      <h1>Hello</h1>
-      <div css={tw`text-green-600`}>World</div>
-      <div tw="text-blue-600">Blue text!</div>
-      <Button>Save</Button>
-      <StyledDiv>Purple!</StyledDiv>
+    <div tw="flex flex-col items-center justify-center h-screen bg-black text-white">
+      <h1>Buttons</h1>
+      <div tw="flex flex-col items-center justify-center">
+        <Button>Save</Button>
+        <a href="/">Back</a>
+      </div>
     </div>
   )
 }
--- packages/playground/src/routes/Home.tsx
+++ packages/playground/src/routes/Home.tsx
@@ -1,110 +1,115 @@
 import React from 'react'
 import 'twin.macro'
-// import { Card } from '../components/Card'
+import { Card } from '../components/Card'
 
 export const Home = () => {
-  return <div>Home</div>
-  // return (
-  //   <section tw="h-screen bg-trueGray-900 text-white font-sans">
-  //     <div tw="container px-5 py-24 mx-auto">
-  //       <div tw="md:flex flex-wrap -m-4">
-  //         <div tw="xl:w-1/3 md:w-1/2 p-4">
-  //           <a
-  //             tw="block border border-trueGray-800 bg-trueGray-900 p-6 rounded-lg hover:bg-red-700 transition-colors duration-500 ease-in-out"
-  //             href="/color"
-  //           >
-  //             <div tw="w-10 h-10 inline-flex items-center justify-center rounded-full bg-red-50 text-red-500 mb-4">
-  //               <svg
-  //                 fill="none"
-  //                 stroke="currentColor"
-  //                 strokeLinecap="round"
-  //                 strokeLinejoin="round"
-  //                 strokeWidth="2"
-  //                 tw="w-6 h-6"
-  //                 viewBox="0 0 24 24"
-  //               >
-  //                 <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-  //               </svg>
-  //             </div>
-  //             <h2 tw="text-lg text-gray-300 font-medium mb-2">Colors</h2>
-  //             <p tw="leading-relaxed text-base text-gray-300">Colors, colors, colors...</p>
-  //           </a>
-  //         </div>
+  return (
+    <section tw="h-screen bg-trueGray-900 text-white font-sans">
+      <div tw="container px-5 py-24 mx-auto">
+        <div tw="md:flex flex-wrap -m-4">
+          <div tw="xl:w-1/3 md:w-1/2 p-4">
+            <a
+              tw="block border border-trueGray-800 bg-trueGray-900 p-6 rounded-lg hover:bg-red-700 transition-colors duration-500 ease-in-out"
+              href="/color"
+            >
+              <div tw="w-10 h-10 inline-flex items-center justify-center rounded-full bg-red-50 text-red-500 mb-4">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  tw="w-6 h-6"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                </svg>
+              </div>
+              <h2 tw="text-lg text-gray-300 font-medium mb-2">Colors</h2>
+              <p tw="leading-relaxed text-base text-gray-300">Colors, colors, colors...</p>
+            </a>
+          </div>
 
-  //         <div tw="xl:w-1/3 md:w-1/2 p-4">
-  //           <a
-  //             tw="block border border-trueGray-800 bg-trueGray-900 p-6 rounded-lg hover:bg-yellow-700 transition-colors duration-500 ease-in-out"
-  //             href="/typography"
-  //           >
-  //             <div tw="w-10 h-10 inline-flex items-center justify-center rounded-full bg-yellow-50 text-yellow-500 mb-4">
-  //               <svg
-  //                 fill="none"
-  //                 stroke="currentColor"
-  //                 strokeLinecap="round"
-  //                 strokeLinejoin="round"
-  //                 strokeWidth="2"
-  //                 tw="w-6 h-6"
-  //                 viewBox="0 0 24 24"
-  //               >
-  //                 <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-  //               </svg>
-  //             </div>
-  //             <h2 tw="text-lg text-gray-300 font-medium mb-2">Typography</h2>
-  //             <p tw="leading-relaxed text-base text-gray-300">
-  //               Typography, typography, typography...
-  //             </p>
-  //           </a>
-  //         </div>
+          <div tw="xl:w-1/3 md:w-1/2 p-4">
+            <a
+              tw="block border border-trueGray-800 bg-trueGray-900 p-6 rounded-lg hover:bg-yellow-700 transition-colors duration-500 ease-in-out"
+              href="/typography"
+            >
+              <div tw="w-10 h-10 inline-flex items-center justify-center rounded-full bg-yellow-50 text-yellow-500 mb-4">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  tw="w-6 h-6"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                </svg>
+              </div>
+              <h2 tw="text-lg text-gray-300 font-medium mb-2">Typography</h2>
+              <p tw="leading-relaxed text-base text-gray-300">
+                Typography, typography, typography...
+              </p>
+            </a>
+          </div>
 
-  //         <div tw="xl:w-1/3 md:w-1/2 p-4">
-  //           <a
-  //             tw="block border border-trueGray-800 bg-trueGray-900 p-6 rounded-lg hover:bg-blue-700 transition-colors duration-500 ease-in-out"
-  //             href="/buttons"
-  //           >
-  //             <div tw="w-10 h-10 inline-flex items-center justify-center rounded-full bg-blue-50 text-blue-500 mb-4">
-  //               <svg
-  //                 fill="none"
-  //                 stroke="currentColor"
-  //                 strokeLinecap="round"
-  //                 strokeLinejoin="round"
-  //                 strokeWidth="2"
-  //                 tw="w-6 h-6"
-  //                 viewBox="0 0 24 24"
-  //               >
-  //                 <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-  //               </svg>
-  //             </div>
-  //             <h2 tw="text-lg text-gray-300 font-medium mb-2">Buttons</h2>
-  //             <p tw="leading-relaxed text-base text-gray-300">Buttons, buttons, buttons...</p>
-  //           </a>
-  //         </div>
+          <div tw="xl:w-1/3 md:w-1/2 p-4">
+            <a
+              tw="block border border-trueGray-800 bg-trueGray-900 p-6 rounded-lg hover:bg-blue-700 transition-colors duration-500 ease-in-out"
+              href="/buttons"
+            >
+              <div tw="w-10 h-10 inline-flex items-center justify-center rounded-full bg-blue-50 text-blue-500 mb-4">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  tw="w-6 h-6"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                </svg>
+              </div>
+              <h2 tw="text-lg text-gray-300 font-medium mb-2">Buttons</h2>
+              <p tw="leading-relaxed text-base text-gray-300">Buttons, buttons, buttons...</p>
+            </a>
+          </div>
 
-  //         <div tw="xl:w-1/3 md:w-1/2 p-4">
-  //           <a
-  //             tw="block border border-trueGray-800 bg-trueGray-900 p-6 rounded-lg hover:bg-teal-700 transition-colors duration-500 ease-in-out"
-  //             href="/inputs"
-  //           >
-  //             <div tw="w-10 h-10 inline-flex items-center justify-center rounded-full bg-teal-50 text-teal-500 mb-4">
-  //               <svg
-  //                 fill="none"
-  //                 stroke="currentColor"
-  //                 strokeLinecap="round"
-  //                 strokeLinejoin="round"
-  //                 strokeWidth="2"
-  //                 tw="w-6 h-6"
-  //                 viewBox="0 0 24 24"
-  //               >
-  //                 <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-  //               </svg>
-  //             </div>
-  //             <h2 tw="text-lg text-gray-300 font-medium mb-2">Inputs</h2>
-  //             <p tw="leading-relaxed text-base text-gray-300">Inputs, inputs, inputs...</p>
-  //           </a>
-  //         </div>
+          <div tw="xl:w-1/3 md:w-1/2 p-4">
+            <a
+              tw="block border border-trueGray-800 bg-trueGray-900 p-6 rounded-lg hover:bg-teal-700 transition-colors duration-500 ease-in-out"
+              href="/inputs"
+            >
+              <div tw="w-10 h-10 inline-flex items-center justify-center rounded-full bg-teal-50 text-teal-500 mb-4">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  tw="w-6 h-6"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                </svg>
+              </div>
+              <h2 tw="text-lg text-gray-300 font-medium mb-2">Inputs</h2>
+              <p tw="leading-relaxed text-base text-gray-300">Inputs, inputs, inputs...</p>
+            </a>
+          </div>
 
-  //         <Card header="Inputs" />
-  //       </div>
-  //     </div>
-  //   </section>
-  // )
+          <Card
+            header="Inputs"
+            text="Inputs, Inputs, Inputs"
+            link="/inputs"
+            color="orange"
+            icon={<svg></svg>}
+          />
+        </div>
+      </div>
+    </section>
+  )
 }
