236dac134de4f2256d2b22ca53d6c1976b15e52e
Nuno Freire <greven.pt@gmail.com>
2020-12-22 20:57:24 +0000

    Typescript working!

--- packages/playground/src/routes/Buttons.tsx
+++ packages/playground/src/routes/Buttons.tsx
@@ -1,17 +1,30 @@
 import React from 'react'
-import 'twin.macro'
+import tw, { styled } from 'twin.macro'
 import { Button } from '@kai/components'
 
-export const Buttons = () => {
-  return (
-    // <div tw="flex flex-col items-center justify-center h-screen bg-black text-white">
-    //   <h1>Buttons</h1>
-    //   <div tw="flex flex-col items-center justify-center">
-    //     <Button>Save</Button>
-    //     <a href="/">Back</a>
-    //   </div>
-    // </div>
+const StyledDiv = styled.div`
+  color: white;
+  ${tw`bg-red-300 p-4 hover:bg-blue-300`}
+`
 
-    <div>Button</div>
+export const Buttons = () => {
+  // return (
+  //   <div tw="flex flex-col items-center justify-center h-screen bg-black text-white">
+  //     <h1>Buttons</h1>
+  //     <div tw="flex flex-col items-center justify-center">
+  //       <Button>Save</Button>
+  //       <a href="/">Back</a>
+  //     </div>
+  //   </div>
+  // )
+
+  return (
+    <div>
+      <h1>Hello</h1>
+      <div css={tw`text-green-600`}>World</div>
+      <div tw="text-blue-600">Blue text!</div>
+      <Button>Save</Button>
+      <StyledDiv>Purple!</StyledDiv>
+    </div>
   )
 }
